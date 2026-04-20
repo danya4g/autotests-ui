18229ab9a736c061fcf8b5197df89c8be2bbbc7e
Daniil Shelepin <@shelepin.daniil>
2026-04-20 10:57:57 +0300

    add some new tests

---
+++ html/tests/test_pytest.py
@@ -0,0 +1,15 @@
+def test_user_login():
+    print("Hello!")
+
+class TestUserLogin:
+    def test_user_login(self):
+        ...
+
+    def test_user_login_2(self):
+        ...
+
+    def test_assert_negative(self):
+        assert (2 + 2) == 5
+
+    def test_assert_positive(self):
+        assert (5 - 2) == 3
