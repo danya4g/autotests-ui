224b390d1c20302841f2171b006b7f9a5ed56657
Daniil Shelepin <@shelepin.daniil>
2026-04-28 10:55:22 +0300

    add new fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,17 @@
 import pytest
 
-from playwright.sync_api import sync_playwright, Page, playwright
+from playwright.sync_api import sync_playwright, Page, playwright, Playwright
 
-@pytest.fixture
-def chromium_page(playwright) -> Page:
+
+@pytest.fixture(scope="session",autouse=True)
+def initialize_browser_state(playwright: Playwright) -> Page:
         browser = playwright.chromium.launch(headless=False)
         context = browser.new_context()
         page = context.new_page()
         yield page
         context.close()
         browser.close()
+
+@pytest.fixture(scope="session",autouse=True)
+def chromium_page_with_state(initialize_browser_state, playwright: Playwright): -> Page:
+        browser = initialize_browser_state
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,29 +1,24 @@
 from playwright.sync_api import sync_playwright, expect
 
-def test_empty_courses_list():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
+def test_empty_courses_list(initialize_browser_state, Page):
+        initialize_browser_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-
-        email_input = page.get_by_test_id("registration-form-email-input").locator("input")
+        email_input = initialize_browser_state.get_by_test_id("registration-form-email-input").locator("input")
         email_input.hover()
         email_input.click()
         email_input.fill("user@gmail.com")
 
-        username_input = page.get_by_test_id("registration-form-username-input").locator("input")
+        username_input = initialize_browser_state.get_by_test_id("registration-form-username-input").locator("input")
         username_input.hover()
         username_input.click()
         username_input.fill("username")
 
-        password_input = page.get_by_test_id("registration-form-password-input").locator("input")
+        password_input = initialize_browser_state.get_by_test_id("registration-form-password-input").locator("input")
         password_input.hover()
         password_input.click()
         password_input.fill("password")
 
-        registration_button = page.get_by_test_id("registration-page-registration-button")
+        registration_button = initialize_browser_state.get_by_test_id("registration-page-registration-button")
         registration_button.click()
 
         context.storage_state(path="browser-state.json")
